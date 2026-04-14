9a8e12afb00081d5c10438f17cf174544c77eb1d
Rafael Augusto <raffery@gmail.com>
2026-04-14 14:02:39 +1200

    added favicon

--- hot-and-flustered/pages/_document.js
+++ hot-and-flustered/pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <link rel="icon" href="/favicon.png" type="image/svg+xml" />
         <link href="https://fonts.googleapis.com/css2?family=Creepster&display=swap" rel="stylesheet" />
       </Head>
       <body>
